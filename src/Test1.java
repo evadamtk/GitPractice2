dfcf34c7153e0bcf25ab4c94f960b17c9f2bf658
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2022-12-11 14:48:06 +0300

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("I suppose that I love Java");
+        System.out.println("I suppose that I love Java :)");
         //gdkdnlskdmvc
         //jvkjdnvkjnzxkv2
         System.out.println("why it is red?");
