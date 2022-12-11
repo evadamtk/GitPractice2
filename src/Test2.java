996788a3e12468149baf2b8294045ee4f77e0e34
evadamtk <112965010+evadamtk@users.noreply.github.com>
2022-12-11 15:26:35 +0300

    Update Test2.java

--- src/Test2.java
+++ src/Test2.java
@@ -8,5 +8,7 @@ System.out.println("James Bond is overrated!")
 // Where is Matt?
 // Where is Bruce Lee?
 // more testing!
+    // I don`t know
+    // but 20 minutes ago I talked with Bruce, he was in his office which is real estate agency
 }
 }
