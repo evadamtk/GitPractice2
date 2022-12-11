c7855c12f2b5a57bd41429704ca5adee2a3999bf
Jamal West <evadamtk@gmail.com>
2022-12-11 14:31:54 +0300

    it is clone file to the repository

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,8 @@
 public class Test1 {
-    public static void main(String args) {
+    public static void main(String[] args) {
         System.out.println("I suppose that I love Java");
         //gdkdnlskdmvc
         //jvkjdnvkjnzxkv2
+        System.out.println("why it is red?");
     }
 }
